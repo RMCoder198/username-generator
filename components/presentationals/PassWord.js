c8f133323f812def43390739a753cbeafd6d7147
RMCoder198 <rsmawari198@gmail.com>
2018-04-01 21:54:28 +0530

    Add password.js

---
+++ components/presentationals/PassWord.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class PassWord extends React.Component {
+  render() {
+    return (
+      <div className="center">
+        <h2 className="text-success ml-lg  text-center">Generate password</h2>
+        <p className="text-md text-center">Cant find any strong password </p>
+        <p className="text-md text-center">Try this!!!!</p>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter words Separated by space"
+        />
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          value={this.props.stateProps.password}
+          readonly
+        />
+      </div>
+    );
+  }
+}
+export default PassWord;
