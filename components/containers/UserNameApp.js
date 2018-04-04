227d45c098ef45635b0b9ab3c7747aa1e879b622
RMCoder198 <rsmawari198@gmail.com>
2018-04-04 12:52:36 +0530

    add dispatch function

--- components/containers/UserNameApp.js
+++ components/containers/UserNameApp.js
@@ -2,8 +2,21 @@ import React from 'react';
 import UserName from '../presentationals/UserName';
 import PassWord from '../presentationals/PassWord';
 import {getState} from 'redux';
+import GenerateUsername from '../../actions/GenerateUsername';
 
 class UsernameApp extends React.Component {
+
+dispatchAction(input){
+  switch (input) {
+    case "generate-username":
+      this.props.store.dispatch(GenerateUsername());
+      break;
+
+
+  }
+}
+
+
   render(){
    const stateProps = this.props.store.getState();
    const userName=<UserName
