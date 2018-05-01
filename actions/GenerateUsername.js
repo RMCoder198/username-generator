47d33a190e094a67344df7798b4f772fde3057af
RMCoder198 <rsmawari198@gmail.com>
2018-05-02 00:15:37 +0530

    import shuffel.js

--- actions/GenerateUsername.js
+++ actions/GenerateUsername.js
@@ -1,5 +1,9 @@
+import Shuffle from './Shuffle';
 function GenerateUsername(change){
   const GENERATE_USERNAME = 'generate-username';
+
+   change= Shuffle(change);
+
   return {
     type: GENERATE_USERNAME,
     username: change,
