6122c7c17e5e8511da5b53b739a436df78a50411
RMCoder198 <rsmawari198@gmail.com>
2018-05-02 00:15:19 +0530

    import shuffel.js

--- actions/GeneratePassword.js
+++ actions/GeneratePassword.js
@@ -1,5 +1,8 @@
+import Shuffle from './Shuffle';
 function GeneratePassword(change){
   const GENERATE_PASSWORD = 'generate-password';
+
+  change= Shuffle(change);
   return {
     type: GENERATE_PASSWORD,
     password: change,
